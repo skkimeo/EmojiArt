92b7e66548d25afca02aa1cf2a6a47f6d5d231d6
sun <skkimeo@gmail.com>
2021-11-09 19:40:01 +0900

    created Model: EmojiArtModel and its extension EmojiArt.Background

---
+++ EmojiArt/Models/EmojiArtModel.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiArtModel.swift
+//  EmojiArt
+//
+//  Created by sun on 2021/11/09.
+//
+
+import Foundation
+
+struct EmojiArtModel {
+    var background: Background = .blank
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable {
+        let text: String
+        var x: Int
+        var y: Int
+        var size: Int
+        var id: Int
+        
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    init() {}
+    
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmoji(text: String, at location: (x: Int, y: Int), size: Int, id: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: location.x, y: location.y, size: size, id: uniqueEmojiId))
+    }
+}
